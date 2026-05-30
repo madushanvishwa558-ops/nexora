553aa2847618e30ced098dfdc421645dee4193e5
madushanvishwa558-ops <madushanvishwa558@gmail.com>
2026-05-30 23:30:32 +0200

    Add speech synthesis to AI response in send function
    Refactor send function to include speech synthesis for AI replies.

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-async function send() {
-  const inputEl = document.getElementById("input");
-  const input = inputEl.value;
-
-  console.log("clicked:", input);
-
-  if (!input) return;
-
-  const chatBox = document.getElementById("chatBox");
-
-  chatBox.innerHTML += "<p><b>You:</b> " + input + "</p>";
-
-  const res = await fetch("/api/chat", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ text: input })
-  });
-
-  const data = await res.json();
-
-  chatBox.innerHTML += "<p><b>AI:</b> " + data.reply + "</p>";
-
-  inputEl.value = "";
-}
+document.getElementById("loading-msg").innerHTML =const speech = new SpeechSynthesisUtterance(data.reply);
+speech.lang = "en-US";
+speech.rate = 1;
+window.speechSynthesis.speak(speech);
